5b4064ba0399dffe482eb5c35740b8ec5c5d010a
Louis Knight-Webb <louis@bloop.ai>
2025-08-15 11:33:47 +0100

    Commit changes from coding agent for task attempt 009251c6-0595-4baf-be5f-684488a8326c

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
-import '@uiw/react-codemirror/dist/codemirror.css';
 import { ClickToComponent } from 'click-to-react-component';
 import * as Sentry from '@sentry/react';
 
