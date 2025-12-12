19da21dba4f75c68ef237fc57c43b8facdd587eb
Britannio Jarrett <britanniojarrett@gmail.com>
2025-12-12 20:59:45 +0000

    review story ui

--- remote-frontend/src/AppRouter.tsx
+++ remote-frontend/src/AppRouter.tsx
@@ -2,12 +2,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import InvitationPage from './pages/InvitationPage'
 import InvitationCompletePage from './pages/InvitationCompletePage'
+import ReviewPage from './pages/ReviewPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/review/:id',
+    element: <ReviewPage />,
   },
   {
     path: '/invitations/:token/accept',
