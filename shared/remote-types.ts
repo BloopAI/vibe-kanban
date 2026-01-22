0ece816354b27a4861d43f22ff1341c0ad309a5a
Gabriel Gordon-Hall <ggordonhall@gmail.com>
2026-01-22 18:44:03 +0000

    types

--- shared/remote-types.ts
+++ shared/remote-types.ts
@@ -36,6 +36,10 @@ export type IssuePriority = "urgent" | "high" | "medium" | "low";
 
 export type WorkspacePrStatus = "open" | "merged" | "closed";
 
+export type PullRequestStatus = "open" | "merged" | "closed";
+
+export type PullRequest = { id: string, url: string, number: number, status: PullRequestStatus, merged_at: string | null, merge_commit_sha: string | null, target_branch_name: string, issue_id: string, workspace_id: string | null, created_at: string, updated_at: string, };
+
 export type UserData = { user_id: string, first_name: string | null, last_name: string | null, username: string | null, };
 
 export enum MemberRole { ADMIN = "ADMIN", MEMBER = "MEMBER" }
