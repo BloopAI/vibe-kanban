3ab4e4cf3a349a716a18ef1ef929c510c42d2ac5
Tianhao Zhou <tianhaozhou95@icloud.com>
2026-03-21 08:27:58 -0700

    feat: add firebase config (#2)

--- yukika/app/lib/main.dart
+++ yukika/app/lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'core/routing/app_router.dart';
 import 'core/theme/app_theme.dart';
+import 'firebase_options.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const YukikaApp());
+void main() async {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    try {
+      if (Firebase.apps.isEmpty) {
+        await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
+      }
+    } catch (e) {
+      debugPrint("Firebase initialization error: $e");
+    }
+
+    runApp(const YukikaApp());
+  }, (error, stack) {
+    debugPrint("Uncaught error: $error");
+  });
 }
 
 class YukikaApp extends StatelessWidget {
